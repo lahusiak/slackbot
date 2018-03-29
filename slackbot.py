9211a8886957f28e4c1bd4853e4448c1195eeea9
lahusiak <laryssa.husiak@buzzfeed.com>
2018-03-29 11:30:56 -0500

    Fixed syntax errors

--- slackbot.py
+++ slackbot.py
@@ -10,17 +10,17 @@ DEFAULT_PORT = 8080
 
 class SlackHandler(RequestHandler):
     def get(self):
-        self.write(“It works!”)
+        self.write("It works!")
 
 
 def make_app():
     return Application([
-        (r’/’, SlackHandler),
+        (r'/', SlackHandler),
     ])
 
 
-if __name__ == ‘__main__’:
-    port = os.environ.get(‘PORT’, DEFAULT_PORT)
+if __name__ == '__main__':
+    port = os.environ.get('PORT', DEFAULT_PORT)
     app = make_app()
     app.listen(port)
     IOLoop.instance().start()
